6b9b7c47775a7b6cc34a5c2daf748bdb5f2e851c
Priyanshu Batham <priyanshubatham24733@gmail.com>
2026-05-28 11:30:55 +0530

    added sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+
 import App from './App';
 import './index.css';
+
+Sentry.init({
+  dsn: "https://d30c25baa35647e9d83c4b8a1ab1aab7@o4511465929441280.ingest.de.sentry.io/4511465938813008",
+
+  sendDefaultPii: true,
+
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 1.0,
+
+  tracePropagationTargets: [
+    "localhost",
+    /^https:\/\/server\.devsunited\.in/,
+  ],
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+
+  enableLogs: true,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
